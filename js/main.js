b77a8ef546eb43ba229b57fdf1bf6074a3e3b28c
Rian Marlon <rianmarlon20@yahoo.com>
2020-03-29 10:35:19 -0300

    Adicionando ícone do menu (hamburguer menu)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 var body = document.querySelector("body");
 
 body.classList.remove("no-js");
-body.classList.add("js")
+body.classList.add("js");
+
+var btnMenu = document.querySelector(".header__btnMenu");
+btnMenu.removeAttribute("style");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
